8ee0e4308204b27ba6423c0e001043c54e31b860
jorgerosal <yortrosal@gmail.com>
2021-03-07 19:21:18 +0800

    update sample snippets

--- sample/getVariations.js
+++ sample/getVariations.js
@@ -6,8 +6,8 @@ const commonParameters = {
   'AccessKey'  : ck.AWS_ACCESS_KEY,
   'SecretKey'  : ck.AWS_SECRET_KEY,
   'PartnerTag' : ck.AWS_TAG, // yourtag-20
-  'PartnerType': 'Associates', // Optional. Default value is Associates. 
-  'Marketplace': 'www.amazon.com' // Optional. Default value is US.
+  'PartnerType': 'Associates', // Default value is Associates. 
+  'Marketplace': 'www.amazon.com' // Default value is US. Note: Host and Region are predetermined based on the marketplace value. There is no need for you to add Host and Region as soon as you specify the correct Marketplace value. If your region is not US or .com, please make sure you add the correct Marketplace value.
 }
 
 const requestParameters = {
